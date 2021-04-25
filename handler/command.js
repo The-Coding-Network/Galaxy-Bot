f28c7c9835557fffbb4a58b0d783983c20fc6805
jameshaswellukcom <69436519+jameshaswellukcom@users.noreply.github.com>
2021-04-25 09:16:12 +0100

    more advanced command handler

--- handler/command.js
+++ handler/command.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 
 module.exports = (client, Discord) => {
-    const command_files = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
-    for(const file of command_files) {
-        const command = require(`../commands/${file}`);
-        if(command.name){
-            client.commands.set(command.name, command);
-        } else {
-        continue;
+    const load_dir = (dirs) =>{
+        const command_files = fs.readdirSync(`./commands/${dirs}`).filter(file => file.endsWith('.js'));
+        
+        for(const file of command_files){
+            const command = require(`../commands/${dirs}/${file}`);
+            client.commands.set(command.name, command)
         }
+        
     }
+    ['moderation', 'other'].forEach(e => load_dir(e));
+
 }
